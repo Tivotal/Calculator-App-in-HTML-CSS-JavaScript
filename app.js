ad870667d713e0c611606fd16f4c0a74d7941794
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-06-12 10:20:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+/* Created by Tivotal */
+
+let screen = document.querySelector(".screen");
+let btns = document.querySelectorAll(".button");
+let string = "";
+
+//looping through btns
+btns.forEach((btn) => {
+  //click event listener for each button
+  btn.addEventListener("click", (e) => {
+    //if clicked btn is equals
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      screen.value = string;
+    } else if (e.target.innerHTML === "AC") {
+      //if AC button clicked
+      //clearing the string
+      string = "";
+      screen.value = string;
+    } else if (e.target.innerHTML === "DEL") {
+      //if del button clicked
+      //deleting last digit of string
+      string = string.substring(0, string.length - 1);
+      screen.value = string;
+    } else {
+      //other any button clicked
+      string += e.target.innerHTML;
+      screen.value = string;
+    }
+  });
+});
